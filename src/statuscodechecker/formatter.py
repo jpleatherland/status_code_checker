fb227b23578e6d5b67f6b670fc5c46cb01dd0edf
Jack Leatherland <jpl92@live.co.uk>
2025-11-07 09:06:43 +0000

    update

--- src/statuscodechecker/formatter.py
+++ src/statuscodechecker/formatter.py
@@ -1,2 +1,6 @@
+from statuscodechecker.status_codes import CodeInfo
+from typing import Mapping
+
+
 def format(to_format: Mapping[int, CodeInfo]):
-    
+    return
--- src/statuscodechecker/status_code_checker.py
+++ src/statuscodechecker/status_code_checker.py
@@ -1,4 +1,4 @@
-from status_codes import status_codes
+from statuscodechecker.status_codes import status_codes
 
 def explain_status_code(status_codes_to_explain: list[int]):
     for status_code in status_codes_to_explain:
@@ -6,5 +6,6 @@ def explain_status_code(status_codes_to_explain: list[int]):
         if code_info:
             print(f"{status_code} - {code_info['title']}: {code_info['description']}")
         else:
-            print(f"{status_code} - Unknown status code, it may be a customer error defined on the server")
-
+            print(
+                f"{status_code} - Unknown status code, it may be a customer error defined on the server"
+            )
